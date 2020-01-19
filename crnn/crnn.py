f24fd1376755e3a4ae69a2690d99a0a83bce8009
dbofseuofhust <846503548@qq.com>
2020-01-19 19:16:24 +0800

    first commit

---
+++ crnn/crnn.py
@@ -0,0 +1,117 @@
+import torch.nn as nn
+
+
+class BidirectionalLSTM(nn.Module):
+
+    def __init__(self, nIn, nHidden, nOut):
+        super(BidirectionalLSTM, self).__init__()
+
+        self.rnn = nn.LSTM(nIn, nHidden, bidirectional=True)
+        self.embedding = nn.Linear(nHidden * 2, nOut)
+
+    def forward(self, input):
+        recurrent, _ = self.rnn(input)
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
+
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(T, b, -1)
+
+        return output
+
+
+class CRNN(nn.Module):
+
+    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+        super(CRNN, self).__init__()
+        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+
+        ks = [3, 3, 3, 3, 3, 3, 2]
+        ps = [1, 1, 1, 1, 1, 1, 0]
+        ss = [1, 1, 1, 1, 1, 1, 1]
+        nm = [64, 128, 256, 256, 512, 512, 512]
+
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i),
+                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i),
+                               nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
+        convRelu(2, True)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x4x16
+        convRelu(4, True)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3),
+                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
+        convRelu(6, True)  # 512x1x16
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(512, nh, nh),
+            BidirectionalLSTM(nh, nh, nclass))
+
+    def forward(self, input):
+        # conv features
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = self.rnn(conv)
+
+        return output
+
+if __name__ == '__main__':
+
+	imagepath = './test.jpg'
+
+	img_h = opt.img_h
+	use_gpu = opt.use_gpu
+	modelpath = opt.modelpath
+	char_set = open('char_std_5990.txt', 'r', encoding='utf-8').readlines()
+	char_set = ''.join([ch.strip('\n') for ch in char_set[1:]] + ['卍'])
+	n_class = len(char_set)
+
+	model = crnn.CRNN(img_h, 1, n_class, 256)
+	if torch.cuda.is_available and use_gpu:
+		model.cuda()
+
+	if os.path.exists(modelpath):
+		print('Load model from "%s" ...' % modelpath)
+		model.load_state_dict(torch.load(modelpath))
+		print('Done!')
+
+	image = Image.open(imagepath).convert('L')
+	(w,h) = image.size
+	size_h = 32
+	ratio = size_h / float(h)
+	size_w = int(w * ratio)
+	# keep the ratio
+	transform = resizeNormalize((size_w, size_h))
+	image = transform(image)
+	image = image.unsqueeze(0)
+	if torch.cuda.is_available and use_gpu:
+		image = image.cuda()
+	model.eval()
+	preds = model(image)
+	preds = preds.max(2)[1]
+	preds = preds.squeeze()
+	pred_text = decode(preds,char_set)
+	print('predict == >',pred_text)
---
+++ reference.py
@@ -0,0 +1,2 @@
+# https://github.com/opconty/pytorch_ctpn
+# https://github.com/ooooverflow/chinese-ocr
